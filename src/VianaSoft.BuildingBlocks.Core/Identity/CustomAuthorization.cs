1b7259b0867770d108f9c57effdf8c128b6b3912
VianaSoft Ltda <vianasoft@outlook.com>
2023-07-28 07:58:51 -0300

    Adicionando ClaimsAuthorizeAttribute

---
+++ src/VianaSoft.BuildingBlocks.Core/Identity/CustomAuthorization.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace VianaSoft.BuildingBlocks.Core.Identity
+{
+    public class CustomAuthorization
+    {
+        public static bool ValidateUserClaims(HttpContext context, string claimType, string claimValue)
+        {
+            return context.User.Identity.IsAuthenticated &&
+                   context.User.Claims.Any(x => x.Type == claimType && x.Value.Contains(claimValue));
+        }
+    }
+}
